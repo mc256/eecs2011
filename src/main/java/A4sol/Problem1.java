de947bc551872c50af64d3f9093a548800bbd7aa
masterchan <masterchan.256@gmail.com>
2016-11-25 11:50:28 -0500

    update

--- src/main/java/A4sol/Problem1.java
+++ src/main/java/A4sol/Problem1.java
@@ -47,102 +47,146 @@ public class Problem1 {
 			System.out.printf("A - {%3d,%3d}  - %3d[%3d]\n", aLeft, aRight, aMid, a[aMid]);
 			System.out.printf("B - {%3d,%3d}  - %3d[%3d]\n", bLeft, bRight, bMid, b[bMid]);
 
+			boolean aTakeLeft, aTakeMid, aTakeRight;
+			boolean bTakeLeft, bTakeMid, bTakeRight;
+			aTakeLeft = aTakeMid = aTakeRight = false;
+			bTakeLeft = bTakeMid = bTakeRight = false;
+
 			if (a[aMid] < b[bMid]) {
 				// Possible Range
 				int aLower = aMid;
 				int aUpper = aMid + bMid;
 				int bLower = aUpper + 1;
 				int bUpper = a.length + bMid;
-				
-				//Choose Partitions
-				boolean aTakeLeft, aTakeMid, aTakeRight;
-				boolean bTakeLeft, bTakeMid, bTakeRight;
-				aTakeLeft = aTakeMid = aTakeRight = false;
-				bTakeLeft = bTakeMid = bTakeRight = false;
-				
-				if (k >= aLower && k <= aUpper){
+
+				// Choose Partitions
+				if (k >= aLower && k <= aUpper) {
 					aTakeMid = true;
 				}
-				if (k < aLower){
+				if (k < aUpper) {
 					aTakeLeft = true;
 				}
-				if (k > aUpper){
+				if (k > aLower) {
 					aTakeRight = true;
 				}
 
-				if (k >= bLower && k <= bUpper){
+				if (k >= bLower && k <= bUpper) {
 					bTakeMid = true;
 				}
-				if (k < bLower){
+				if (k < bUpper) {
 					bTakeLeft = true;
 				}
-				if (k > bUpper){
+				if (k > bLower) {
 					bTakeRight = true;
 				}
-				System.out.printf("A %s%s%s - {%3d,%3d}\n", (aTakeLeft?"o":"x"),(aTakeMid?"o":"x"),(aTakeRight?"o":"x"), aLower, aUpper);
-				System.out.printf("B %s%s%s - {%3d,%3d}\n", (bTakeLeft?"o":"x"),(bTakeMid?"o":"x"),(bTakeRight?"o":"x"), bLower, bUpper);
-				
-				//Adjust Range
-				if (aTakeLeft && !aTakeMid && !aTakeRight){
-					aRight = aMid - 1;
-				}
-				if (aTakeLeft && aTakeMid && !aTakeRight){
-					aRight = aMid;
-				}
-				if (!aTakeLeft && aTakeMid && aTakeRight){
-					aLeft = aMid;
-				}
-				if (!aTakeLeft && !aTakeMid && aTakeRight){
-					aLeft = aMid + 1;
-				}
-				
-				
+				System.out.printf("A %s%s%s - {%3d,%3d}\n", (aTakeLeft ? "o" : "x"), (aTakeMid ? "o" : "x"), (aTakeRight ? "o" : "x"), aLower, aUpper);
+				System.out.printf("B %s%s%s - {%3d,%3d}\n", (bTakeLeft ? "o" : "x"), (bTakeMid ? "o" : "x"), (bTakeRight ? "o" : "x"), bLower, bUpper);
 
-				if (bTakeLeft && !bTakeMid && !bTakeRight){
-					bRight = bMid - 1;
+				// Adjust Range
+				if (!aTakeRight) {
+					aRight = aMid;
+					if (!aTakeMid) {
+						aRight = aMid - 1;
+					}
 				}
-				if (bTakeLeft && bTakeMid && !bTakeRight){
+				if (!aTakeLeft) {
+					aLeft = aMid;
+					if (!aTakeMid) {
+						aLeft = aMid + 1;
+					}
+				}
+
+				if (!bTakeRight) {
 					bRight = bMid;
+					if (!bTakeMid) {
+						bRight = bMid - 1;
+					}
 				}
-				if (!bTakeLeft && bTakeMid && bTakeRight){
+				if (!bTakeLeft) {
 					bLeft = bMid;
+					if (!bTakeMid) {
+						bLeft = bMid + 1;
+					}
 				}
-				if (!bTakeLeft && !bTakeMid && bTakeRight){
-					bLeft = bMid + 1;
-				}
-				
+
 			} else {
-				// handle B
-				/*
+				// Possible Range
 				int bLower = bMid;
-				int bUpper = bMid + aMid - 1;
+				int bUpper = bMid + aMid;
 				int aLower = bUpper + 1;
 				int aUpper = b.length + aMid;
-				System.out.printf("B - {%3d,%3d}\n", bLower, bUpper);
-				if (k < bLower) {
-					bRight = bMid - 1;
-				} else if (k > bUpper) {
-					bLeft = bMid + 1;
-				} else {
+
+				// Choose Partitions
+				if (k >= aLower && k <= aUpper) {
+					aTakeMid = true;
+				}
+				if (k < aUpper) {
+					aTakeLeft = true;
+				}
+				if (k > aLower) {
+					aTakeRight = true;
+				}
+
+				if (k >= bLower && k <= bUpper) {
+					bTakeMid = true;
+				}
+				if (k < bUpper) {
+					bTakeLeft = true;
+				}
+				if (k > bLower) {
+					bTakeRight = true;
+				}
+				System.out.printf("B %s%s%s - {%3d,%3d}\n", (bTakeLeft ? "o" : "x"), (bTakeMid ? "o" : "x"), (bTakeRight ? "o" : "x"), bLower, bUpper);
+				System.out.printf("A %s%s%s - {%3d,%3d}\n", (aTakeLeft ? "o" : "x"), (aTakeMid ? "o" : "x"), (aTakeRight ? "o" : "x"), aLower, aUpper);
+
+				// adjust Range
+				if (!bTakeRight) {
 					bRight = bMid;
+					if (!bTakeMid) {
+						bRight = bMid - 1;
+					}
 				}
-				if (k < aLower){
-					aRight = aMid - 1;
-				}else if (k > aUpper){
-					aLeft = aMid + 1;
-				}else{
+				if (!bTakeLeft) {
+					bLeft = bMid;
+					if (!bTakeMid) {
+						bLeft = bMid + 1;
+					}
+				}
+
+				if (!aTakeRight) {
+					aRight = aMid;
+					if (!aTakeMid) {
+						aRight = aMid - 1;
+					}
+				}
+				if (!aTakeLeft) {
 					aLeft = aMid;
+					if (!aTakeMid) {
+						aLeft = aMid + 1;
+					}
 				}
-				*/
 			}
 
 			if (aLeft == aRight) {
-				System.out.printf("A:index%d - element%d\n", aLeft, a[aLeft]);
-				return;
+				if (aTakeMid) {
+					System.out.printf("A:index[%d] - element[%d]\n", aLeft, a[aLeft]);
+					return;
+				}
+				if (bTakeMid) {
+					System.out.printf("B:index[%d] - element[%d]\n", bLeft, b[bLeft]);
+					return;
+				}
+
 			}
 			if (bLeft == bRight) {
-				System.out.printf("B:index%d - element%d\n", bLeft, b[bLeft]);
-				return;
+				if (aTakeMid) {
+					System.out.printf("A:index[%d] - element[%d]\n", aLeft, a[aLeft]);
+					return;
+				}
+				if (bTakeMid) {
+					System.out.printf("B:index[%d] - element[%d]\n", bLeft, b[bLeft]);
+					return;
+				}
 			}
 		}
 
@@ -150,8 +194,8 @@ public class Problem1 {
 
 	public static void main(String[] args) {
 
-		// int s[] = new int[] { 3, 12, 18, 20, 28, 37, 41, 44, 45, 48 };
-		// int t[] = new int[] { 4, 5, 6, 7, 8, 13, 15, 21, 23, 27 };
+		//int s[] = new int[] { 3, 12, 18, 20, 28, 37, 41, 44, 45, 48 };
+		//int t[] = new int[] { 4, 5, 6, 7, 8, 13, 15, 21, 23, 27 };
 
 		int s[] = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 		int t[] = new int[] { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
@@ -162,7 +206,7 @@ public class Problem1 {
 		System.out.printf("S = %s\n", arrayToString(s));
 		System.out.printf("T = %s\n", arrayToString(t));
 
-		findLargest(s, t, 2);
+		findLargest(s, t, 5);
 	}
 
 }
